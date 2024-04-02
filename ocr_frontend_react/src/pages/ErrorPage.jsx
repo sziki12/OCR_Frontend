c0deaf8b647847de00e288d213d0ca1f7982fcb5
sziki12 <szikilevi01@gmail.com>
2024-04-02 22:26:55 +0200

    Root and Error page styleing progress

--- ocr_frontend_react/src/pages/ErrorPage.jsx
+++ ocr_frontend_react/src/pages/ErrorPage.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import MainSection from "../components/utils/MainSection";
+import {Typography,Paper} from "@mui/material";
+import {NavLink} from "react-router-dom";
 
 
 export default function ErrorPage() {
     return (
-        <div>
-            <p>404 Not Found</p>
-        </div>
+        <MainSection>
+            <div className={"flex justify-center"}>
+                <Paper className={"p-6"} elevation={0}>
+                    <Typography sx={{ fontSize: 36 }}>404 Page Not Found</Typography>
+                    <div className={"flex justify-center"}>
+                        <NavLink className={"text-red-600"} to={"/"}>Please return to a valid Page</NavLink>
+                    </div>
+                </Paper>
+            </div>
+        </MainSection>
 
     );
 }
--- ocr_frontend_react/src/pages/Root.jsx
+++ ocr_frontend_react/src/pages/Root.jsx
@@ -1,17 +1,63 @@
-import logo from '../logo.svg';
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import MainSection from "../components/utils/MainSection";
+import {Card, CardContent,Box,Typography,Stack,Paper,} from "@mui/material";
+import {faComputer, faDatabase, faImage} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-
-//props.name
 function Root() {
+    const location = useLocation();
   return (
       <MainSection>
         <div>
-          <Outlet/>
+            {
+                (location.pathname === "/")
+                ?
+                    <>
+                        <RootContent key={"root_content"}/>
+                    </>
+                :
+                    <>
+                        <Outlet key={"outlet"}/>
+                    </>
+            }
         </div>
       </MainSection>
   );
+}
+
+function RootContent()
+{
+    return(
+        <Paper className={"px-6 pt-6 pb-16"}>
+            <Typography sx={{fontSize:18}} component="div">Main Features</Typography>
+            <Stack spacing={6} direction="row" justifyContent={"center"}>
+                <Card sx={{width:200}}>
+                        <CardContent>
+                            Store Your Receipts Online
+                            <br/>
+                            <div className={"flex justify-center"}><FontAwesomeIcon size={"2xl"} icon={faDatabase} /></div>
+                        </CardContent>
+                    </Card>
+                <Card sx={{width:200}}>
+                    <CardContent>
+                        Extract Receipt from Image
+                        <br/>
+                        <div className={"flex justify-center"}><FontAwesomeIcon size={"2xl"} icon={faComputer} /></div>
+
+
+                    </CardContent>
+                </Card>
+                <Card sx={{width:200}}>
+                    <CardContent>
+                        Save the Image with the data
+                        <br/>
+                        <div className={"flex justify-center"}><FontAwesomeIcon size={"2xl"} icon={faImage} /></div>
+                    </CardContent>
+                </Card>
+            </Stack>
+        </Paper>
+
+    )
 }
 
 export default Root;
